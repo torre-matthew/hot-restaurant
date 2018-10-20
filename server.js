17135ed84c3cddd0e48aba456031fe49b7e5693a
Quy Nguyen <nguyenqb24@gmail.com>
2018-10-20 10:39:51 -0400

    server.js created

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "reservations.html"));
+  });
+  
+  app.get("/all", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
